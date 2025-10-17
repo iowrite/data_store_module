ff1b7941bb29b7039238759fffe8c4ec207dc8c7
zzy <iowrite@icloud.com>
2025-10-17 17:32:41 +0800

    1. 优化默认宏选项处理宏

--- src/default_config_to_user_config.sh
+++ src/default_config_to_user_config.sh
@@ -9,14 +9,18 @@ your are useing default config, \
 please define DATA_STORE_USER_CONFIG_FILE to your own config file,\
 or delete this warning, edit this file accord your project\"" > "$USER_FILE"
 
-# 处理文件：保留整个文件结构，只移除指定区域内的#ifndef/#endif
-sed -E '
-/\/\/ script process start/,/\/\/ script process end/{
-    /^#ifndef [A-Z_]+$/,/^#endif$/{
-        /^#ifndef [A-Z_]+$/d
-        /^#endif$/d
-    }
-}
-' "$FILE" >> "$USER_FILE"
+echo "#ifndef XXX_USER_CONFIG_FILE" >> "$USER_FILE"
+echo "#define XXX_USER_CONFIG_FILE" >> "$USER_FILE"
+
+
+# 处理文件：只提取//script process start 与 //script process end之间的内容
+sed -n '/\/\/ script process start/,/\/\/ script process end/p' "$FILE" | \
+sed '/\/\/ script process start/d; /\/\/ script process end/d' >> "$USER_FILE"
+
+
+
+echo "#endif" >> "$USER_FILE"
+
 
 echo "处理完成，结果保存在: $USER_FILE"
+
